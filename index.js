698f34adfca7d5e69f9ac438c0e3059d5866ad04
mk-hasan <mkhasan6891@gmail.com>
2022-05-23 00:20:02 +0600

    get method implement for get all product

--- index.js
+++ index.js
@@ -3,11 +3,47 @@ const cors = require('cors');
 const port = process.env.PORT || 5000;
 require('dotenv').config();
 const app = express();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 
 // middleware
 app.use(cors());
 app.use(express.json());
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.4lnss.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        // console.log('database connected');
+        const productCollection = client.db('motoParts').collection('product');
+
+        app.get('/product', async (req, res) => {
+            const query = {};
+            const cursor = productCollection.find(query); //project add for without slots
+            const products = await cursor.toArray();
+            res.send(products);
+        });
+
+        // get single  product
+        app.get('/product/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const product = await productCollection.findOne(query);
+            res.send(product);
+        });
+
+    }
+    finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
 
 app.get('/', (req, res) => {
     res.send('Alhamdulillah');
