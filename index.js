4db11daf09a763edd2d9db05e5303ae6ced911ad
mk-hasan <mkhasan6891@gmail.com>
2022-05-24 12:09:36 +0600

    order product api created

--- index.js
+++ index.js
@@ -21,10 +21,14 @@ async function run() {
         await client.connect();
         // console.log('database connected');
         const productCollection = client.db('motoParts').collection('product');
+        const orderCollection = client.db('motoParts').collection('order');
+        // const productCollection = client.db('motoParts').collection('product');
+        // const productCollection = client.db('motoParts').collection('product');
 
+        //    get all product
         app.get('/product', async (req, res) => {
             const query = {};
-            const cursor = productCollection.find(query); //project add for without slots
+            const cursor = productCollection.find(query);
             const products = await cursor.toArray();
             res.send(products);
         });
@@ -36,6 +40,41 @@ async function run() {
             const product = await productCollection.findOne(query);
             res.send(product);
         });
+
+
+        app.put('/product/:id', async (req, res) => {
+            const id = req.params.id;
+            const updateProduct = req.body;
+            console.log(updateProduct);
+            const query = { _id: ObjectId(id) }
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: {
+                    availableQty: updateProduct.availableQty
+                }
+            }
+
+            const result = await productCollection.updateOne(query, updateDoc, options)
+            res.send(result)
+        });
+
+        // load all orders
+        app.get('/order', async (req, res) => {
+            // const query = {};
+            const email = req.query.email;
+            const query = { email: email };
+            // const cursor = orderCollection.find(query);
+            const orders = await orderCollection.find(query).toArray();
+            res.send(orders);
+        });
+
+        // for post orders
+        app.post("/order", async (req, res) => {
+            const order = req.body;
+            const result = await orderCollection.insertOne(order);
+            res.send(result);
+        })
+
 
     }
     finally {
